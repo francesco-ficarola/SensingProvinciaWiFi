98a94c9ef1c4933351ef4db98647513962433ff3
Francesco Ficarola <ficarola@dis.uniroma1.it>
2013-07-11 12:24:06 +0200

    A first structure of PcsProxy.

--- src/sensingprovinciawifi/pcs/PcsProxy.java
+++ src/sensingprovinciawifi/pcs/PcsProxy.java
@@ -1,6 +1,19 @@
 package sensingprovinciawifi.pcs;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Arrays;
+
+import org.apache.log4j.Logger;
+
+import sensingprovinciawifi.core.WifiConnection;
+import sensingprovinciawifi.pcs.utils.PcsConstants;
+
 public class PcsProxy implements Runnable {
+	
+	private static Logger logger = Logger.getLogger(PcsProxy.class);
 	
 	public PcsProxy() {
 		new Thread(this).start();
@@ -9,6 +22,41 @@ public class PcsProxy implements Runnable {
 	@Override
 	public void run() {
 		
+		boolean initialized = false;
+		DatagramSocket serverSocket = null;
+		
+		while(!initialized) {
+			try {
+				serverSocket = new DatagramSocket(PcsConstants.UDP_SERVER_PORT);
+				initialized = true;
+				logger.info("PCS UDP Server has been initialized.");
+			} catch (SocketException e) {
+				logger.error("PCS UDP Server has not been initialized: " + e.getMessage(), e);
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					logger.error(e1.getMessage(), e1);
+				}
+			}
+		}
+		
+		byte[] receiveData = new byte[PcsConstants.PCS_PACKET_SIZE];
+		while(true) {
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			try {
+				serverSocket.receive(receivePacket);
+				byte[] data = receivePacket.getData();
+				logger.info(Arrays.toString(data));
+				
+				//TODO: byte array parsing
+				
+				//TODO: sending data
+				while(!WifiConnection.connectToWifi());
+				
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+			}
+		}
 	}
 
 }
---
+++ src/sensingprovinciawifi/pcs/utils/PcsConstants.java
@@ -0,0 +1,6 @@
+package sensingprovinciawifi.pcs.utils;
+
+public class PcsConstants {
+	public static final Integer UDP_SERVER_PORT = 2342;
+	public static final Integer PCS_PACKET_SIZE = 32;
+}
